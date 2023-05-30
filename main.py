423427c81d4a132df9a17526b5910569be6892fc
Johnny Papercut <taco.megazord@gmail.com>
2023-05-30 15:43:13 -0500

    auth

--- main.py
+++ main.py
@@ -112,14 +112,15 @@ def oauth2callback(profile: str):
         client_config={
             "web": {
                 "client_id": client_id,
+                "project_id": "zirekyle-main",
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
                 "client_secret": client_secret,
                 "redirect_uris": [
                     "http://localhost:5000/oauth2callback/Zirekyle",
                     "https://xcad.zirekyle.com/oauth2callback/Zirekyle",
-                    "https://xcad-akurivnfnq-uc.a.run.app/oauth2callback/Zirekyle"
-                ],
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://accounts.google.com/o/oauth2/token"
+                    "https://xcad-akurivnfnq-uc.a.run.app/oauth2callback/Zirekyle"]
             }
         },
         scopes=["https://www.googleapis.com/auth/youtube"], state=state)
